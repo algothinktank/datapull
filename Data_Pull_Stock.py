3f8e88553f51a60d6bbd5b2b02a14b82918a28bf
ebtrader <jsiddique@gmail.com>
2020-05-19 22:49:40 -0400

    Add files via upload

---
+++ Data_Pull_Stock.py
@@ -0,0 +1,167 @@
+
+import argparse
+import logging
+import os.path
+import time
+import datetime
+from ibapi.utils import iswrapper
+
+from StockSamples import ContractSamples
+
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+# types
+from ibapi.common import *  # @UnusedWildImport
+from ibapi.order import *  # @UnusedWildImport
+
+def SetupLogger():
+    if not os.path.exists("log"):
+        os.makedirs("log")
+
+    time.strftime("pyibapi.%Y%m%d_%H%M%S.log")
+
+    recfmt = '(%(threadName)s) %(asctime)s.%(msecs)03d %(levelname)s %(filename)s:%(lineno)d %(message)s'
+
+    timefmt = '%y%m%d_%H:%M:%S'
+
+    # logging.basicConfig( level=logging.DEBUG,
+    #                    format=recfmt, datefmt=timefmt)
+    logging.basicConfig(filename=time.strftime("log/pyibapi.%y%m%d_%H%M%S.log"),
+                        filemode="w",
+                        level=logging.INFO,
+                        format=recfmt, datefmt=timefmt)
+    logger = logging.getLogger()
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    logger.addHandler(console)
+
+
+def printWhenExecuting(fn):
+    def fn2(self):
+        print("   doing", fn.__name__)
+        fn(self)
+        print("   done w/", fn.__name__)
+
+    return fn2
+
+# ! [socket_init]
+class TestApp(EWrapper, EClient):
+    def __init__(self):
+        EWrapper.__init__(self)
+        EClient.__init__(self, wrapper=self)
+        # ! [socket_init]
+        self.nKeybInt = 0
+        self.started = False
+        self.nextValidOrderId = None
+        self.permId2ord = {}
+        self.globalCancelOnly = False
+        self.simplePlaceOid = None
+        self._my_errors = {}
+
+    @iswrapper
+    # ! [connectack]
+    def connectAck(self):
+        if self.asynchronous:
+            self.startApi()
+
+    # ! [connectack]
+
+
+        # we can start now
+        self.start()
+
+    def start(self):
+        if self.started:
+            return
+
+        self.started = True
+
+        if self.globalCancelOnly:
+            print("Executing GlobalCancel only")
+            self.reqGlobalCancel()
+        else:
+            print("Executing requests")
+            self.tickDataOperations_req()
+
+            print("Executing requests ... finished")
+
+    def keyboardInterrupt(self):
+        self.nKeybInt += 1
+        if self.nKeybInt == 1:
+            self.stop()
+        else:
+            print("Finishing test")
+            self.done = True
+
+    @iswrapper
+    # ! [error]
+    def error(self, reqId: TickerId, errorCode: int, errorString: str):
+        super().error(reqId, errorCode, errorString)
+        print("Error. Id:", reqId, "Code:", errorCode, "Msg:", errorString)
+        errormsg = "IB error id %d errorcode %d string %s" % (reqId, errorCode, errorString)
+        self._my_errors = errormsg
+
+    @iswrapper
+    def winError(self, text: str, lastError: int):
+        super().winError(text, lastError)
+
+    @printWhenExecuting
+    def tickDataOperations_req(self):
+        self.reqMarketDataType(MarketDataTypeEnum.DELAYED_FROZEN)
+        #self.reqTickByTickData(19002, ContractSamples.USStock(), "AllLast", 0, False)
+        self.reqTickByTickData(19001, ContractSamples.USStock(), "Last", 0, True)
+
+    @iswrapper
+    # ! [tickbytickalllast]
+    def tickByTickAllLast(self, reqId: int, tickType: int, time: int, price: float,
+                          size: int, tickAttribLast: TickAttribLast, exchange: str,
+                          specialConditions: str):
+        super().tickByTickAllLast(reqId, tickType, time, price, size, tickAttribLast,
+                                  exchange, specialConditions)
+        if tickType == 1:
+            print("Last.", end='')
+        else:
+            print("AllLast.", end='')
+        print(" ReqId:", reqId,
+              "Time:", datetime.datetime.fromtimestamp(time).strftime("%Y%m%d %H:%M:%S"),
+              "Price:", price, "Size:", size, "Exch:", exchange,
+              "Spec Cond:", specialConditions, "PastLimit:", tickAttribLast.pastLimit, "Unreported:",
+              tickAttribLast.unreported)
+    # ! [tickbytickalllast]
+
+def main():
+    SetupLogger()
+    logging.getLogger().setLevel(logging.ERROR)
+
+    cmdLineParser = argparse.ArgumentParser("api tests")
+    # cmdLineParser.add_option("-c", action="store_True", dest="use_cache", default = False, help = "use the cache")
+    # cmdLineParser.add_option("-f", action="store", type="string", dest="file", default="", help="the input file")
+    cmdLineParser.add_argument("-p", "--port", action="store", type=int,
+                               dest="port", default=7497, help="The TCP port to use")
+    cmdLineParser.add_argument("-C", "--global-cancel", action="store_true",
+                               dest="global_cancel", default=False,
+                               help="whether to trigger a globalCancel req")
+    args = cmdLineParser.parse_args()
+    print("Using args", args)
+    logging.debug("Using args %s", args)
+
+    try:
+        app = TestApp()
+        if args.global_cancel:
+            app.globalCancelOnly = True
+        # ! [connect]
+        app.connect("127.0.0.1", args.port, clientId=0)
+        # ! [connect]
+        print("serverVersion:%s connectionTime:%s" % (app.serverVersion(),
+                                                      app.twsConnectionTime()))
+        # ! [clientrun]
+        app.run()
+        # ! [clientrun]
+    except:
+        raise
+    finally:
+        app.dumpTestCoverageSituation()
+        app.dumpReqAnsErrSituation()
+
+if __name__ == "__main__":
+    main()
